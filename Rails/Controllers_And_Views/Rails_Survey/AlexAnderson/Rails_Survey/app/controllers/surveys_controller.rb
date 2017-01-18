3b65ad3aee15c980c13565561ce0e082769579f6
Dwei99 <alexanderanderson1@gmail.com>
2017-01-18 10:37:40 -0600

    finished survey rails assignment

--- Rails/Controllers_And_Views/Rails_Survey/AlexAnderson/Rails_Survey/app/controllers/surveys_controller.rb
+++ Rails/Controllers_And_Views/Rails_Survey/AlexAnderson/Rails_Survey/app/controllers/surveys_controller.rb
@@ -1,4 +1,5 @@
 class SurveysController < ApplicationController
+
   def index
     render 'surveys/index'
   end
@@ -8,7 +9,11 @@ class SurveysController < ApplicationController
     @location = params[:location]
     @language = params[:language]
     @comment = params[:comment]
-
+    if(!session[:count])
+      session[:counter] = 0
+    end
+    session[:counter] = session[:counter] + 1
+    flash[:sucess] = "Thanks for submitting this form! You have submitted this form ",session[:counter],"times"
     render 'show'
   end
 end
