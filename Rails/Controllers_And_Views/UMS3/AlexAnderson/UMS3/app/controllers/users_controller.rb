38552314cb0adeec240a99c74121ccce86202a8b
Dwei99 <alexanderanderson1@gmail.com>
2017-01-18 14:39:32 -0600

    initial commit for UMS3, have show user functionality working"

---
+++ Rails/Controllers_And_Views/UMS3/AlexAnderson/UMS3/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    # puts @users.second.first_name
+    render 'index'
+  end
+  def new
+    render 'new'
+  end
+  def create
+    @user =  User.create( first_name: params[:first_name], last_name: params[:last_name], email_address: params[:email], password: params[:password])
+    @errors = @user.errors.messages
+    if @errors.length > 0
+      render '/users/new'
+    else
+      redirect_to '/users'
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+    puts @user.first_name
+    render 'show'
+  end
+end
