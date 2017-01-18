c8bbc4f96abea69eb776d83e872658a73cd84841
Dwei99 <alexanderanderson1@gmail.com>
2017-01-18 17:41:02 -0600

    initial commit for product dashboard assignment

---
+++ Rails/Controllers_And_Views/Products_Dashboard/AlexAnderson/product_dashboard/app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render '/products/index'
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
