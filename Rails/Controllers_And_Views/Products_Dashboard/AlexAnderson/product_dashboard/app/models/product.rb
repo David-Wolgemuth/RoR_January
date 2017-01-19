a9e236fb361c5aec9ea25101db57da59c8cb7f05
Dwei99 <alexanderanderson1@gmail.com>
2017-01-19 00:38:39 -0600

    created category model and updated index page to display category data

--- Rails/Controllers_And_Views/Products_Dashboard/AlexAnderson/product_dashboard/app/models/product.rb
+++ Rails/Controllers_And_Views/Products_Dashboard/AlexAnderson/product_dashboard/app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
+  belongs_to :category
   validates :name, presence: { message: "Name must be entered"}
   validates :description, presence: { message: "Description must be entered"}
   validates :pricing, presence: { message: "Pricing must be entered"}
-  
+
 end
--- Rails/Controllers_And_Views/Products_Dashboard/AlexAnderson/product_dashboard/db/schema.rb
+++ Rails/Controllers_And_Views/Products_Dashboard/AlexAnderson/product_dashboard/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118225434) do
+ActiveRecord::Schema.define(version: 20170119054646) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +25,9 @@ ActiveRecord::Schema.define(version: 20170118225434) do
     t.float    "pricing"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
 end
