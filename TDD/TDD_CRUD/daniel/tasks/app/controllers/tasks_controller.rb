0d6f3241495393fe6153608300c19045e6317cf9
Daniel Smith <dsmit192@mail.depaul.edu>
2017-01-20 16:26:48 -0600

    finished tasks, you should check it out

--- TDD/TDD_CRUD/daniel/tasks/app/controllers/tasks_controller.rb
+++ TDD/TDD_CRUD/daniel/tasks/app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
 
   def index
+    @the_task = Task.new
     @all_tasks = Task.all
   end
 
@@ -8,8 +9,27 @@ class TasksController < ApplicationController
     params.permit
     @new_task = Task.new(params[:task].permit!)
     @new_task.save
-    redirect_to '/tasks'
+    redirect_to '/tasks', :notice => "You added a task!"
   end
 
+  def edit
+    @the_task = Task.find (params[:id])
+  end
+
+  def update
+    params.permit
+    task = Task.find (params[:id])
+    if task.update_attributes params[:task].permit!
+      redirect_to tasks_path, :notice => "You have updated a task"
+    else
+      redirect_to :back, :notice => "You cannot update a blank task"
+    end
+  end
+
+  def destroy
+    Task.destroy params[:id]
+    redirect_to :back, :notice => "Task has been deleted"
+
+  end
 
 end
--- TDD/TDD_CRUD/daniel/tasks/spec/requests/tasks_spec.rb
+++ TDD/TDD_CRUD/daniel/tasks/spec/requests/tasks_spec.rb
@@ -1,10 +1,13 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "Tasks", type: :request do
+describe "Tasks" do
+  before do
+    @task = Task.create :task => 'go to bed'
+  end
+
   feature "GET /tasks" do
 
     it 'display the tasks' do
-      @all_tasks = Task.create :task => 'go to bed'
 
       visit tasks_path
       page.should have_content 'go to bed'
@@ -15,11 +18,51 @@ RSpec.describe "Tasks", type: :request do
       fill_in 'Task', :with => 'go to work'
       click_button 'Create Task'
 
-      current_path.expect == tasks_path
-      page.shold have_content 'go to work'
+      current_path.should == tasks_path
+      page.should have_content 'go to work'
 
       save_and_open_page
     end
-
   end
+
+    feature "PUT /tasks" do
+      it "edits a task" do
+        visit tasks_path
+        click_link 'Edit'
+
+        current_path.should == edit_task_path(@task)
+
+        find_field('Task').value.should == 'go to bed'
+
+        fill_in 'Task', :with => 'updated task'
+        click_button 'Update Task'
+
+        current_path.should == tasks_path
+
+        page.should have_content 'updated task'
+        page.should have_content "You have updated a task"
+      end
+
+      it "should not update an empty task" do
+        visit tasks_path
+        click_link 'Edit'
+
+        fill_in 'Task', :with => ''
+        click_button 'Update Task'
+
+        current_path.should == edit_task_path(@task)
+
+        page.should have_content "You cannot update a blank task"
+      end
+    end
+
+    feature "DELETE /tasks" do
+      it "should delete a task" do
+        visit tasks_path
+        find("#task_#{@task.id}").click_link 'Delete'
+        page.should have_content 'Task has been deleted'
+        page.should have_no_content 'go to bed'
+      end
+    end
+
 end
